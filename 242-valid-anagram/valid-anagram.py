4df310511d911954baddfb568923489022ba1811
Himanshu sen <113094302+Himanshu-sen@users.noreply.github.com>
2025-08-06 18:34:33 +0530

    Time: 21 ms (62.38%) | Memory: 14.5 MB (10.16%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -5,12 +5,15 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        s = list(s)
-        t = list(t)
-        s.sort()
-        t.sort()
-        if s==t:
-            return True
-        else:
+        if len(s) != len(t):
             return False
+        counts, countt = {}, {}
+        for i in range(len(s)):
+            counts[s[i]] = 1 + counts.get(s[i],0)
+            countt[t[i]] = 1 + countt.get(t[i],0)
+        
+        for c in counts:
+            if counts[c]!= countt.get(c,0):
+                return False
+        return True 
         
