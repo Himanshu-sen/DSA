0dc3396f1a3d9758d051930456079d21e4dc4c69
Himanshu sen <113094302+Himanshu-sen@users.noreply.github.com>
2025-08-06 18:36:58 +0530

    Time: 23 ms (56.73%) | Memory: 12.5 MB (83.00%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -5,15 +5,5 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        if len(s) != len(t):
-            return False
-        counts, countt = {}, {}
-        for i in range(len(s)):
-            counts[s[i]] = 1 + counts.get(s[i],0)
-            countt[t[i]] = 1 + countt.get(t[i],0)
-        
-        for c in counts:
-            if counts[c]!= countt.get(c,0):
-                return False
-        return True 
+        return Counter(s) == Counter(t)
         
