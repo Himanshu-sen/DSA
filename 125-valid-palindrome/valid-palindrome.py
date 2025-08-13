13feec34d8207b25c0798f770d03437d424b1722
Himanshu sen <113094302+Himanshu-sen@users.noreply.github.com>
2025-08-13 17:49:29 +0530

    Time: 137 ms (21.98%) | Memory: 13.1 MB (55.65%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        ns = ''
+        for i in s:
+            if i.isalnum():
+                ns+=i.lower()
+        return ns == ns[::-1]
+        
