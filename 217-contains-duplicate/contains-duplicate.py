3c1df5ca38b5e04af1dda08a46426eec106d5483
Himanshu sen <113094302+Himanshu-sen@users.noreply.github.com>
2025-08-19 16:25:26 +0530

    Time: 22 ms (36.17%) | Memory: 26.1 MB (6.90%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -4,10 +4,4 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        hashset = set()
-        for i in nums:
-            if i not in hashset:
-                hashset.add(i)
-            else:
-                return True
-        return False
+        return len(nums) != len(set(nums))
